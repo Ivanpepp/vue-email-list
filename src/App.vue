7c708596895540dbbfd4c32afb4abbb0a8f3ec8c
Ivanpepp <nastroivan3@gmail.com>
2021-09-28 14:19:56 +0200

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Boolean Logo" src="./assets/img/logo-small.png">
+    <HelloWorld msg="Benvenuto nel generatore casuale di mail by Boolean Api" :mailRandom='mailRandom'/>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: function(){
+    return{
+      mailRandom: '',
+    }
+  },
+  mounted(){
+    const result=[];
+    let self=this;
+    for(let i= 0 ; i<10; i++){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response)=>{
+      
+      
+         result.push(response.data.response);
+        
+      })
+      
+    }
+    self.mailRandom = result;
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +51,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  img{
+    width: 200px;
+    transition: 1s cubic-bezier(0.19, 1, 0.22, 1);
+    &:hover{
+          transform: scale(0.8);
+    }
+  }
+  
 }
 </style>
